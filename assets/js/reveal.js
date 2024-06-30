541ae033c48c5e887077b1c1bf47f2bf4246eeb5
Lucas Dias Leandro <147958304+lucasserdev@users.noreply.github.com>
2024-06-30 16:56:20 -0300

    Initial commit

---
+++ assets/js/reveal.js
@@ -0,0 +1,14 @@
+window.revelar = ScrollReveal({ reset: false });
+
+
+revelar.reveal('.efect-socials', {
+    duration: 2000,
+    distance: '300px',
+    delay: 300,
+    origin: 'left',
+});
+
+revelar.reveal('.infos', {
+    duration: 1000,
+    distance: '110px',
+});
